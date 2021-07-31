f8fdc6da67c9bc42534d5a8926043401b9c07265
Spixa <spixa1937@gmail.com>
2021-07-31 15:04:00 +0430

    Added cooldown, and safety for empty sends!
    ayup

---
+++ src/server/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+
+    // bool invld_res = (chatSend_str.find_first_not_of(" \t\n\v\f\r") == std::string::npos);
+
+    // if (invld_res) {
+    //     std::string message = "6\x01Stop sending empty shit.";
+    //     send(message.c_str(),message.length() + 1, client);
+    //     return false;
+    // }
