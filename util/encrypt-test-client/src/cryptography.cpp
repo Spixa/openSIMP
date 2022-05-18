a35fffd4887f855a8dbad9142a19c3b397eb2052
Spixa <74703416+Spixa@users.noreply.github.com>
2022-05-18 18:04:25 +0430

    edited another file

--- util/encrypt-test-client/src/cryptography.cpp
+++ util/encrypt-test-client/src/cryptography.cpp
@@ -1,11 +1,11 @@
-      #include "cryptography.h"
+#include "cryptography.h"
 
 Cryptography::Cryptography() : rng(), kp(new Botan::RSA_PrivateKey(rng, 4096)) {
       enc = Cipher_Mode::create("AES-256/GCM", ENCRYPTION);
       dec = Cipher_Mode::create("AES-256/GCM", DECRYPTION); 
 } 
 
-std::string Cryptography::encrypt(const std::string & str, const char keyhex[]) {
+secure_vector<uint8_t> Cryptography::encrypt(const std::string & str, const char keyhex[]) {
              // defining variables
              secure_vector<uint8_t> input;
              secure_vector<uint8_t> key;
@@ -15,6 +15,7 @@ std::string Cryptography::encrypt(const std::string & str, const char keyhex[]) 
                   std::cout << ERROR_MSG << '\n' << "\twhat(): "
                   << e.what() << '\n';
             }
+
             input = secure_vector<uint8_t>(str.data(), str.data() + str.length());
             secure_vector<uint8_t> iv = this->rng.random_vec(enc->default_nonce_length()); 
 
@@ -22,10 +23,10 @@ std::string Cryptography::encrypt(const std::string & str, const char keyhex[]) 
          enc->set_key(key);
          enc->start(iv);
          enc->finish(input);
-         return std::string(hex_encode(iv) + hex_encode(input)); 
-        
+         std::string res_str{hex_encode(iv) + hex_encode(input)}; 
+         return hex_decode_locked(res_str);
 }
-std::unordered_map<std::string, std::string> Cryptography::decrypt(const std::string& str, const char keyhex[]) {
+std::unordered_map<std::string, secure_vector<uint8_t>> Cryptography::decrypt(const secure_vector<uint8_t>& ct, const char keyhex[]) {
           secure_vector<uint8_t> output;
           secure_vector<uint8_t> iv;
           secure_vector<uint8_t> key;
@@ -35,14 +36,14 @@ std::unordered_map<std::string, std::string> Cryptography::decrypt(const std::st
                   std::cout << ERROR_MSG << '\n' << "\twhat(): "
                   << e.what() << '\n';
             }
-            std::string ivstr;
-            for(int i = 0; i < 24; i++) {
-                 ivstr+= str.c_str()[i];
+            
+            for (int i = 0; i < 12; i++) {
+                  iv.push_back(ct[i]);
             }
-            std::string encstr = str;
-            encstr.erase(0, 24);
-            iv = secure_vector<uint8_t>(hex_decode_locked(ivstr));
-            output = secure_vector<uint8_t>(hex_decode_locked(encstr));
+
+            for (int i = 12; i < ct.size(); i++) {
+                  output.push_back(ct[i]);
+            }
 
 
            // start to decompile 
@@ -51,15 +52,15 @@ std::unordered_map<std::string, std::string> Cryptography::decrypt(const std::st
 
            dec->finish(output, 0); 
 
-          std::unordered_map<std::string, std::string> map;
-          map["iv"] = hex_encode(iv);
-          map["msg"] = hex_encode(output); 
+          std::unordered_map<std::string, secure_vector<uint8_t>> map;
+          map["iv"] = iv;
+          map["msg"] = output; 
 
          return map;
-}     
+}
+
 
 void Cryptography::pushNewClientKey(const std::string& key) {
-
       keys.push_back(RSA_PublicKey( BigInt(hex_decode(key)), EXPONENT_MAX));
 
 }
@@ -68,19 +69,22 @@ void Cryptography::pushNewClientKey(const BigInt& bi) {
       keys.push_back(RSA_PublicKey( bi, EXPONENT_MAX));
 }
 
-const std::string Cryptography::RSA_encrypt(size_t iterator, const std::string& plaintext) {
+const std::vector<uint8_t> Cryptography::RSA_encrypt(size_t iterator, const std::string& plaintext) {
       // Encrypter device
+
       PK_Encryptor_EME enc_dev(keys[iterator], rng, "PKCS1v15");
+      
 
       secure_vector<uint8_t> pt(plaintext.data(), plaintext.data() + plaintext.length());
       std::vector<uint8_t> ct = enc_dev.encrypt(pt, rng);
       
-      return hex_encode(ct);
+
+      return ct;
 }
 
-secure_vector<uint8_t> Cryptography::RSA_decrypt(const std::string& given) {
+secure_vector<uint8_t> Cryptography::RSA_decrypt(secure_vector<uint8_t> given) {
       // Decrypt deviice
       PK_Decryptor_EME dec_dev(*kp, rng, "PKCS1v15");
 
-      return dec_dev.decrypt(hex_decode(given));
+      return dec_dev.decrypt(given);
 }
